0731b11f2ce40594e13093834a83a6416e33fc3a
Martin Dedek <dedek-m@email.cz>
2023-09-21 09:29:55 +0200

    Applied the base of strategy pattern

--- scraper.py
+++ scraper.py
@@ -5,8 +5,163 @@ import time
 import argparse
 import os
 import asyncio
+from abc import ABC, abstractmethod
 
-class Scraper:
+class ScraperBase(ABC):
+    @abstractmethod
+    def scrape_all_pages(self):
+        pass
+
+
+class ScraperMixin:
+    # Contains all the common methods so that they can be reused in individual Scraper implementations but arent forced by the ScraperBase abstract class
+
+    def _get_image_url(self, img_element):
+        # Get all urls with the width parameter from the data-srcset, then split them by commas, get the first one since all the urls are the same
+        try:
+            img_url = img_element['src']
+
+        except KeyError:
+            imgurls_datascrset = img_element['data-srcset']
+            imgurls_list = [url.strip() for url in imgurls_datascrset.split(',')]
+            img_url = imgurls_list[0].split(" ")[0]
+
+        return img_url
+
+    def _get_page_range(self, last_page, start_from = 1):
+        # Generates a range of page numbers starting from 'start_from' up to and including 'last_page'.
+        page_range = range(start_from, last_page +1)
+        return page_range
+    
+    def _extract_last_page_number(self, content):
+        soup = BeautifulSoup(content, 'html.parser')
+
+        pagination = soup.find("nav", class_ = "pagination")
+        last_page_list_item = pagination.find_all('li', class_='pagination-link')[-1]
+        last_page_link = last_page_list_item.find('a')
+        try:
+            last_page = int(last_page_link.text.strip())
+            return last_page
+        
+        except ValueError as e:
+            print("Error converting last page number to int.")
+            raise e
+        
+    def _extract_page(self, content):
+        """
+        Scrapes content from a specific page URL and returns a list of results.
+
+        Args:
+            page_url (str): The URL of the page to be scraped.
+
+        Returns:
+            list: A list of tuples containing scraped data (title, summary, URL, image URL) from the page.
+        """
+        
+        soup = BeautifulSoup(content, 'html.parser')
+        article_list = soup.find('div', class_=["entry-list", "entries-articles"])
+        items = article_list.find_all('div', class_ = "entry-inner")
+        results = []
+
+        for item in items:
+            title = item.find('h3', class_ = "entry-title").text.strip()
+            summary = item.find('p', class_ = "entry-body__text").text.strip()
+            article_url = item.find('h3', class_ = "entry-title").find('a')['href']
+
+            img_elem = item.find('a', class_ = "image").find('img')
+            img_url = self._get_image_url(img_elem)
+
+            results.append((title, summary, article_url, img_url))
+
+        return results
+        
+
+class Scraper(ScraperBase, ScraperMixin):
+    # Initial synchronous Scraper implementation
+    def __init__(self, base_url: str, max_retries : int = 3, retry_interval : float = 5, between_req_wait : float = 5):
+        """
+        Initializes a Scraper instance.
+
+        Args:
+            base_url (str): The base URL of the website to be scraped.
+            max_retries (int, optional): The maximum number of retries on request failure. Default is 3.
+            retry_interval (int, optional): The time interval (in seconds) between retries. Default is 5.
+            between_req_wait (int, optional): The time interval (in seconds) between page loads. Default is 5.
+        """        
+        self.base_url = base_url
+        self.max_retries = max_retries
+        self.retry_interval = retry_interval
+        self.between_req_wait = between_req_wait 
+
+    def _get_last_page_number(self):
+        # Extracts and returns the last page number from the website's pagination.
+        response = self._get_response(self.base_url)
+        return self._extract_last_page_number(response.content)
+            
+    
+    def _get_response(self, page_url):
+        # Sends a GET request to the specified 'page_url' and handles retries according to the 'max_retries' and 'retry_interval' specified during object creation.
+        retry_counter = 0
+        exception = None
+        while retry_counter < self.max_retries:
+            try:
+                response = requests.get(page_url)
+                response.raise_for_status()  
+                return response
+            
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to retrieve page {page_url}, sleeping for {self.retry_interval}")
+                retry_counter += 1
+                time.sleep(self.retry_interval)
+                exception = e
+        raise exception #Exception(f"Failed to retrieve valid response for {page_url}: e")
+                
+        
+
+    def scrape_page(self, page_url):
+        """
+        Scrapes content from a specific page URL and returns a list of results.
+
+        Args:
+            page_url (str): The URL of the page to be scraped.
+
+        Returns:
+            list: A list of tuples containing scraped data (title, summary, URL, image URL) from the page.
+        """
+        
+        print(f'Scraping {page_url}')
+        response = self._get_response(page_url)
+        return self._extract_page(response.content)
+    
+
+    def scrape_all_pages(self):
+        """
+        Scrapes content from all pages of the website and returns a list of results.
+
+        Returns:
+            list: A list of tuples containing scraped data (title, summary, URL, image URL) from all pages.
+        """
+        print(f"Running scraper for {self.base_url} with {self.max_retries} max retries, {self.retry_interval} sec retry interval, {self.between_req_wait} secs between page loads")
+        last_page = self._get_last_page_number()
+        #last_page = 2
+        page_range = self._get_page_range(last_page)
+        print(f'{page_range[-1]} pages found')
+        all_results = []
+
+        for page in page_range:
+            page_url = (f'{self.base_url}?p1400={page}')
+            page_results = self.scrape_page(page_url)
+            all_results.extend(page_results)
+
+            time.sleep(self.between_req_wait)  # Wait between requests
+
+        return all_results
+
+
+
+
+class ScraperAsync(ScraperBase, ScraperMixin):
+    # Scraper implementation utilizing the asyncio asynchronous run
     def __init__(self, base_url : str , semaphore_limit : int = 100, max_retries : int = 3 , retry_interval : float = 5, between_req_wait : float = 5):
         """
         Initializes a Scraper instance.
@@ -27,36 +182,8 @@ class Scraper:
     async def _get_last_page_number(self):
         # Extracts and returns the last page number from the website's pagination.
         response = await self._get_response(self.base_url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        pagination = soup.find("nav", class_ = "pagination")
-        last_page_list_item = pagination.find_all('li', class_='pagination-link')[-1]
-        last_page_link = last_page_list_item.find('a')
-        try:
-            last_page = int(last_page_link.text.strip())
-            return last_page
-        
-        except ValueError as e:
-            print("Error converting last page number to int.")
-            raise e
-            
-    def _get_page_range(self, last_page, start_from = 1):
-        # Generates a range of page numbers starting from 'start_from' up to and including 'last_page'.
-        page_range = range(start_from, last_page +1)
-        return page_range
-
-    
-    def _get_image_url(self, img_element):
-        # Get all urls with the width parameter from the data-srcset, then split them by commas, get the first one since all the urls are the same
-        try:
-            img_url = img_element['src']
-
-        except KeyError:
-            imgurls_datascrset = img_element['data-srcset']
-            imgurls_list = [url.strip() for url in imgurls_datascrset.split(',')]
-            img_url = imgurls_list[0].split(" ")[0]
-
-        return img_url
+        return self._extract_last_page_number(response.content)
+                
     
     async def _get_response(self, page_url):
         # Sends a GET request to the specified 'page_url' and handles retries according to the 'max_retries' and 'retry_interval' specified during object creation.
@@ -100,26 +227,13 @@ class Scraper:
         response = await self._get_response(page_url)
         print(f'Scraping {page_url}')
 
-        soup = BeautifulSoup(response.content, 'html.parser')
-        article_list = soup.find('div', class_=["entry-list", "entries-articles"])
-        items = article_list.find_all('div', class_ = "entry-inner")
-        results = []
-
-        for item in items:
-            title = item.find('h3', class_ = "entry-title").text.strip()
-            summary = item.find('p', class_ = "entry-body__text").text.strip()
-            article_url = item.find('h3', class_ = "entry-title").find('a')['href']
-
-            img_elem = item.find('a', class_ = "image").find('img')
-            img_url = self._get_image_url(img_elem)
-
-            results.append((title, summary, article_url, img_url))
-
-        return results
+        return self._extract_page(response.content)
     
 
-    async def scrape_all_pages(self):
-        loop = asyncio.get_event_loop()
+    def scrape_all_pages(self):
+        return asyncio.run(self._scrape_all_pages_async())
+    
+    async def _scrape_all_pages_async(self):
         last_page = await self._get_last_page_number()
         page_range = range(1, last_page + 1)
         print(f'{last_page} pages found')
@@ -140,6 +254,7 @@ class Scraper:
             all_results.extend(page_results)
 
         return all_results
+
     
 if __name__ == "__main__":
     # Since the script was specifically built for this URL, it is hardcoded here and not passed as an argument
@@ -152,6 +267,7 @@ if __name__ == "__main__":
     parser.add_argument("--wait-time", type=float, default=2, help="Time (in seconds) to wait between page loads")
     parser.add_argument("--semaphore-limit", type=int, default=5, help="Max concurrent coroutines for asyncio semaphore")
     parser.add_argument("--output", default="data/mnamky.csv", help="Path to the output CSV file")
+    parser.add_argument("--no-async", action="store_true", help="Turn of asynchronous run, making the program request one page at a time")
 
     args = parser.parse_args()
 
@@ -163,25 +279,29 @@ if __name__ == "__main__":
             os.makedirs(output_dir, exist_ok=False)
 
     #semaphore_limit = 5
-
-    scraper = Scraper(
-        base_url=base_url,
-        semaphore_limit = args.semaphore_limit,
-        max_retries=args.max_retries,
-        retry_interval=args.retry_time,
-        between_req_wait=args.wait_time
-        )
+    if args.no_async:
+        scraper = Scraper(
+            base_url=base_url,
+            max_retries=args.max_retries,
+            retry_interval=args.retry_time,
+            between_req_wait=args.wait_time
+            )
+    else:       
+        scraper = ScraperAsync(
+            base_url=base_url,
+            semaphore_limit = args.semaphore_limit,
+            max_retries=args.max_retries,
+            retry_interval=args.retry_time,
+            between_req_wait=args.wait_time
+            )
 
     try:
-        results = asyncio.run(scraper.scrape_all_pages())
+        results = scraper.scrape_all_pages()
     except Exception as e:
         print(f"An error occurred: {e}")
         exit()
 
     columns = ['title', 'summary', 'url', 'image_url']
     df = pd.DataFrame(results, columns=columns)
-
-
-
     df.to_csv(output, sep="\t", encoding="UTF-16", index = False)
     print(df)
